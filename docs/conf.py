d30d306972f36f34071448a6e277a2764aae9207
Rodolfo Flores <33376598+rodjfg@users.noreply.github.com>
2025-06-21 13:15:49 -0400

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# docs/conf.py
+
+project = 'neural-analysis'
+author = 'Your Name'
+release = '0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
